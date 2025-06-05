fec444aea234a8e4032e8f9d9d7ae60a4c54834d
Sabari_krishna <sabarikrishna.vijay@adroitminds.com>
2025-06-05 18:36:05 +0530

    bug fix in sortBySelect component

--- src/components/common/select.tsx
+++ src/components/common/select.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ChevronDown } from 'lucide-react';
 import CheckBox from './inputs/CheckBox';
 import clsx from 'clsx';
@@ -15,13 +15,26 @@ const SortBySelect = ({ label, options, className }: SortBySelectProps) => {
   const [selected, setSelected] = useState('');
   const [isOpen, setIsOpen] = useState(false);
 
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
   const handleSelect = (option: string) => {
     setSelected((prev) => (prev === option ? '' : option));
     setIsOpen(false);
   };
+  useEffect(() => {
 
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
   return (
-    <div className="relative max-w-[320px] w-full h-[50px]">
+    <div ref={dropdownRef} className="relative max-w-[320px] w-full h-[50px]">
       {/* Trigger */}
       <button
         type="button"
@@ -29,17 +42,17 @@ const SortBySelect = ({ label, options, className }: SortBySelectProps) => {
         className={clsx("w-full h-[50px] flex justify-between items-center px-4 py-2 border border-gray-200 bg-white rounded-md text-sm ", className)}
       >
         <div>{selected ? <>
-        {<><span className='font-medium'>{label}</span>
-        <span> {selected}</span></>}
-        </> 
-        : <span className='font-medium'>{label}</span> }
+          {<><span className='font-medium'>{label}</span>
+            <span> {selected}</span></>}
+        </>
+          : <span className='font-medium'>{label}</span>}
         </div>
         <ChevronDown className="w-4 h-4" />
       </button>
 
       {/* Dropdown */}
       {isOpen && (
-        <div className="absolute z-10 mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg">
+        <div className="absolute z-10  w-full bg-white border border-gray-300 rounded-md shadow-lg">
           {options.map((option) => (
             <label
               key={option}
